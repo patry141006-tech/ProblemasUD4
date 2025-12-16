7b5662315038fcd539fc9cbd87c76169443f83a1
patry141006-tech <danielaalvarez16012023@gmail.com>
2025-12-16 14:19:19 +0100

    Problema1Terminado

--- ProblemasUD04/src/App.java
+++ ProblemasUD04/src/App.java
@@ -2,9 +2,10 @@ import Problemas.Racional;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Racional r1 = new Racional(1, 2);
-        Racional r2 = new Racional(2, 3);
+        Racional r1 = new Racional(2, 6);
+        Racional r2 = new Racional(1, 3);
         Racional suma = r1.suma(r2);
         System.out.printf("La suma de %s + %s = %s\n", r1, r2, suma);
+        System.out.println(r1.equals(r2));
     }
 }
--- ProblemasUD04/src/Problemas/Racional.java
+++ ProblemasUD04/src/Problemas/Racional.java
@@ -41,13 +41,45 @@ public class Racional {
     public Racional suma(Racional ra) throws Exception {
         int d1 = this.d;
         int d2 = ra.d;
-        int n1= this.n;
-        int n2= ra.n;
+        int n1 = this.n;
+        int n2 = ra.n;
 
-        if(d1 == d2){
-            return new Racional((n1+n2), d1);
-        } else{
-            return new Racional((n1*d2 + n2 * d1), (d1*d2));
+        if (d1 == d2) {
+            return new Racional((n1 + n2), d1);
+        } else {
+            return new Racional((n1 * d2 + n2 * d1), (d1 * d2));
         }
     }
+
+    public Racional suma(Racional ra1, Racional ra2) throws Exception {
+        int d1 = ra1.d;
+        int d2 = ra2.d;
+        int n1 = ra1.n;
+        int n2 = ra2.n;
+
+        if (d1 == d2) {
+            return new Racional((n1 + n2), d1);
+        } else {
+            return new Racional((n1 * d2 + n2 * d1), (d1 * d2));
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Racional other = (Racional) obj;
+        double div1 = (double) n / d;
+        double div2 = ((double) other.n / other.d);
+
+        if (div1 != div2) {
+            return false;
+        }
+        return true;
+    }
+
 }
