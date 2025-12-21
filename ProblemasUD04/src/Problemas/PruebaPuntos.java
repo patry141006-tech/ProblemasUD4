dd7b82b5a82f0d2dd1d03f79db83e9467781b754
patry141006-tech <248350974+patry141006-tech@users.noreply.github.com>
2025-12-21 15:12:13 +0100

    Todo hecho

--- ProblemasUD04/src/Problemas/PruebaPuntos.java
+++ ProblemasUD04/src/Problemas/PruebaPuntos.java
@@ -2,7 +2,15 @@ package Problemas;
 
 public class PruebaPuntos {
     public static void main(String[] args) {
-        Punto2D p1 = new Punto2D(3, 2.5);
-        System.out.println(p1.x + " " +  p1.y);
+
+        Punto2D p1 = new Punto2D(3, 2);
+        Punto2D p2 = new Punto2D(1, -1);
+        Punto2D p3 = p1.suma(p2);
+
+        p2.setX(p1.getX());
+        p2.setY(p1.getY());
+
+        System.out.println(
+                p1 + " es igual a " + p2 + "? " + p1.equals(p2));
     }
 }
--- ProblemasUD04/src/Problemas/Punto2D.java
+++ ProblemasUD04/src/Problemas/Punto2D.java
@@ -1,12 +1,62 @@
 package Problemas;
 
 public class Punto2D {
-    public double x, y;
+    private double x;
+    private double y;
 
     public Punto2D(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public Punto2D() {
+        this(0, 0);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Punto2D suma(Punto2D otro) {
+        return new Punto2D(this.x + otro.x, this.y + otro.y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        // es lo missmo
+        if (this == obj) {
+            return true;
+        }
+
+        // es nulo o distinta clase
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Punto2D otro = (Punto2D) obj;
+
+        if (this.x == otro.x && this.y == otro.y) {
+            return true;
+        }
+        return false;
+    }
+
 }
-
-
